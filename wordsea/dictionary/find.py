40abf63676929ca924ae2313038257b9b01ff49b
Michał Szachniewicz <mszachniewicz@outlook.com>
2024-01-14 18:49:57 +0100

    dict: fix path to templates

--- wordsea/dictionary/find.py
+++ wordsea/dictionary/find.py
@@ -6,6 +6,10 @@ from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 from tqdm import tqdm
 
+import wordsea
+
+TEMPLATES_PATH = Path(wordsea.__file__).parent / "dictionary" / "templates"
+
 
 def find_word(
     word: str, path: str = "/mnt/Sidra/wiktionary/raw-wiktextract-data-clean.json"
@@ -24,13 +28,13 @@ def find_word(
 
 
 def render_definition(entries) -> str:
-    env = Environment(loader=FileSystemLoader("dictionary/templates"))
+    env = Environment(loader=FileSystemLoader(TEMPLATES_PATH))
     template = env.get_template("definition.html")
     return template.render(entries=entries)
 
 
 def render_prompt(word: str, definition: str) -> str:
-    env = Environment(loader=FileSystemLoader("dictionary/templates"))
+    env = Environment(loader=FileSystemLoader(TEMPLATES_PATH))
     template = env.get_template("prompt.html")
     return template.render(word=word, definition=definition)
 
