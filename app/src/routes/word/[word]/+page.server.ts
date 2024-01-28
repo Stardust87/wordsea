7f538a84a2c3c1ea13f1d0bbd2379e7492d31395
Michał Szachniewicz <mszachniewicz@outlook.com>
2024-01-28 19:28:59 +0100

    app: add basic display of mnemonic

---
+++ app/src/routes/word/[word]/+page.server.ts
@@ -0,0 +1,35 @@
+import { mnemonics, gridfs } from "$lib/server/database";
+
+export const load = async ({ params }) => {
+    const { word } = params;
+    const availableWords = await mnemonics.distinct("word");
+
+    const mnemo = await mnemonics.findOne({ word }, { sort: { $natural: -1 } });
+    const images: string[] = [];
+
+    if (mnemo?.images) {
+        for (const image of mnemo.images) {
+            const imageId = image.data;
+            const stream = gridfs.openDownloadStream(imageId);
+
+            const chunks: Buffer[] = [];
+            stream.on('data', (chunk) => {
+                chunks.push(chunk);
+            });
+            stream.on('end', () => {
+                const img = Buffer.concat(chunks).toString('base64');
+                images.push(img);
+            });
+            await new Promise((resolve) => stream.on('end', resolve));
+        }
+    }
+
+
+    return {
+        ...params,
+        prompt: mnemo?.prompt,
+        explanation: mnemo?.explanation,
+        images,
+        availableWords,
+    }
+}
