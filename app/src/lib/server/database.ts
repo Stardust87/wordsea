ab9907bd3e288cbbaa0d59b7990a935b5c2bcff2
Michał Szachniewicz <mszachniewicz@outlook.com>
2024-01-28 19:28:59 +0100

    app: add basic display of mnemonic

---
+++ app/src/lib/server/database.ts
@@ -0,0 +1,8 @@
+import { MONGODB_URL, MONGODB_DB_NAME } from "$env/static/private";
+import { GridFSBucket, MongoClient } from "mongodb";
+
+const client = new MongoClient(MONGODB_URL);
+const db = client.db(MONGODB_DB_NAME);
+
+export const gridfs = new GridFSBucket(db);
+export const mnemonics = db.collection("mnemonics");
