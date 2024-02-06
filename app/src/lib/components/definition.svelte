978583c1c6c35fddfdfbaac09ec42edfd8ba9a8c
Anna Klimiuk <ania.klimiuk411@gmail.com>
2024-02-06 21:30:28 +0100

    app: display definition, upgrade layout

---
+++ app/src/lib/components/definition.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import type { Meaning } from '$lib/types/Meaning';
+	export let word: string = '';
+	export let meanings: Meaning[] = [];
+</script>
+
+<ul class="list-inside list-none">
+	{#each meanings as meaning}
+		<li>
+			<span class="text-2xl">
+				{word}
+				<span class="text-xl italic">({meaning.pos})</span>
+			</span>
+			<ol class="ml-4 list-inside list-decimal">
+				{#each meaning.senses as sense}
+					<li>
+						{sense.gloss}
+						{#if sense.examples}
+							<ul class="ml-8 list-inside list-disc">
+								{#each sense.examples as example}
+									<li>{example.text}</li>
+								{/each}
+							</ul>
+						{/if}
+					</li>
+				{/each}
+			</ol>
+		</li>
+	{/each}
+</ul>
---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<main class="container mx-auto px-2">
+	<slot />
+</main>
