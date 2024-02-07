75ff46e82ef92c1016599c039570d4a5e29875c8
Anna Klimiuk <ania.klimiuk411@gmail.com>
2024-02-07 15:48:42 +0100

    app: add dark theme

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,7 +3,14 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      square: 'square',
+      roman: 'upper-roman',
+    }
   },
+  darkMode: 'class',
   plugins: [],
 }
-
