ec3c3990250a3b57a7a3b691e689b23919e21077
Anna Klimiuk <ania.klimiuk411@gmail.com>
2024-02-08 16:16:22 +0100

    app: add styling to definition cards

--- app/src/lib/components/definition.svelte
+++ app/src/lib/components/definition.svelte
@@ -20,30 +20,35 @@
 	};
 </script>
 
-<ol class="list-roman list-inside py-4">
+<ol class="list-roman max-w-screen-lg list-inside pb-8 pt-4">
 	{#each meanings as meaning}
-		<li>
-			<span class="text-xl italic">{meaning.pos} </span>
-			{#if filterForms(meaning.forms, meaning.pos).length > 0}
-				<span class="text-lg italic text-slate-800 dark:text-slate-400">
-					&mdash; {filterForms(meaning.forms, meaning.pos)}
-				</span>
-			{/if}
+		<div
+			class="border-1 m-3 rounded-md border-solid bg-slate-200/30 p-2 shadow-lg
+			shadow-slate-300 hover:bg-slate-200 dark:border-transparent dark:bg-slate-700/50 dark:shadow-sm dark:shadow-slate-700 dark:hover:bg-slate-800/50"
+		>
+			<li>
+				<span class="text-xl italic">{meaning.pos} </span>
+				{#if filterForms(meaning.forms, meaning.pos).length > 0}
+					<span class="text-lg italic text-slate-800 dark:text-slate-400">
+						&mdash; {filterForms(meaning.forms, meaning.pos)}
+					</span>
+				{/if}
 
-			<ol class="ml-4 list-inside list-decimal">
-				{#each meaning.senses as sense}
-					<li>
-						{sense.gloss}
-						{#if sense.examples}
-							<ul class="ml-8 list-inside list-disc">
-								{#each sense.examples as example}
-									<li>{example.text}</li>
-								{/each}
-							</ul>
-						{/if}
-					</li>
-				{/each}
-			</ol>
-		</li>
+				<ol class="ml-4 list-inside list-decimal">
+					{#each meaning.senses as sense}
+						<li>
+							{sense.gloss}
+							{#if sense.examples}
+								<ul class="ml-8 list-inside list-disc">
+									{#each sense.examples as example}
+										<li>{example.text}</li>
+									{/each}
+								</ul>
+							{/if}
+						</li>
+					{/each}
+				</ol>
+			</li>
+		</div>
 	{/each}
 </ol>
