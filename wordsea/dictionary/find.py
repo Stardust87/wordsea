5e39af3c39e930f743b765cf6c31a149704927b6
Anna Klimiuk <ania.klimiuk411@gmail.com>
2024-02-14 15:27:50 +0100

    refactor: add tqdm to finding script

--- wordsea/dictionary/find.py
+++ wordsea/dictionary/find.py
@@ -2,6 +2,8 @@ import json
 import logging
 from collections import defaultdict
 
+from tqdm import tqdm
+
 from wordsea.db import Meaning
 
 logging.basicConfig(
@@ -16,15 +18,25 @@ def find_words(
     found_words = defaultdict(list)
     not_found = []
 
-    for word in words:
+    found = 0
+    pbar = tqdm(enumerate(words), desc="Finding", total=len(words))
+    for _, word in pbar:
+        pbar.set_postfix(
+            {
+                "found": f"{found}",
+                "not_found": f"{len(not_found)}",
+            }
+        )
         if query_words := Meaning.objects(word=word):
             found_words[str(word)] = [
                 json.loads(word.to_json()) for word in query_words
             ]
+            found += 1
         else:
             not_found.append(word)
 
     if not_found:
         logging.warning(f"The following words were not found: {', '.join(not_found)}")
 
+    print(f"Found {found} words from {len(words)}")
     return found_words
