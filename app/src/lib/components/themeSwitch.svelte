340bfb7c0eb997fb4705b4cbb1895c85102865f0
Michał Szachniewicz <mszachniewicz@outlook.com>
2024-02-18 17:34:32 +0100

    app: fix blinking theme switch on reload

--- app/src/lib/components/themeSwitch.svelte
+++ app/src/lib/components/themeSwitch.svelte
@@ -1,56 +1,75 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
 
-	let darkMode = true;
+	let dark: boolean;
+	let hidden = true;
 
-	function handleSwitchDarkMode() {
-		darkMode = !darkMode;
+	onMount(() => {
+		dark = document.documentElement.classList.contains('dark');
 
-		localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+		hidden = false;
+		const matcher = window.matchMedia('(prefers-color-scheme: dark)');
+		matcher.addEventListener('change', handleChange);
+		return () => matcher.removeEventListener('change', handleChange);
+	});
 
-		darkMode
-			? document.documentElement.classList.add('dark')
-			: document.documentElement.classList.remove('dark');
+	function handleChange({ matches: dark }: MediaQueryListEvent) {
+		if (!localStorage.theme) {
+			setMode(dark);
+		}
 	}
 
-	if (browser) {
-		if (
-			localStorage.theme === 'dark' ||
-			(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-		) {
+	function toggle() {
+		setMode(!dark);
+	}
+
+	function setMode(value: boolean) {
+		dark = value;
+
+		if (dark) {
 			document.documentElement.classList.add('dark');
-			darkMode = true;
 		} else {
 			document.documentElement.classList.remove('dark');
-			darkMode = false;
+		}
+
+		localStorage.theme = dark ? 'dark' : 'light';
+
+		if (window.matchMedia(`(prefers-color-scheme: ${localStorage.theme})`).matches) {
+			localStorage.removeItem('theme');
 		}
 	}
 </script>
 
-<div class="pt-1">
-	<input
-		checked={darkMode}
-		on:click={handleSwitchDarkMode}
-		type="checkbox"
-		id="theme-toggle"
-	/><label for="theme-toggle" />
-</div>
+<svelte:head>
+	<script>
+		if (
+			localStorage.theme === 'dark' ||
+			(!localStorage.theme && window.matchMedia('(prefers-color-scheme: dark)').matches)
+		) {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	</script>
+</svelte:head>
 
-<style lang="postcss">
-	#theme-toggle {
-		@apply invisible;
-	}
-
-	#theme-toggle + label {
-		@apply inline-block size-6 cursor-pointer rounded-full duration-300 content-[''];
-	}
-
-	#theme-toggle:not(:checked) + label {
-		@apply bg-amber-400;
-	}
-
-	#theme-toggle:checked + label {
-		@apply bg-transparent;
-		box-shadow: inset -8px -6px 1px 1px #ddd;
-	}
-</style>
+<button
+	class="{dark
+		? 'ring-offset-gray-700 focus:ring-gray-400'
+		: 'ring-offset-white focus:ring-yellow-400'} m-1 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2"
+	class:hidden
+	on:click={toggle}
+>
+	<!-- moon icon -->
+	<svg class="size-7 text-gray-400" class:hidden={!dark} viewBox="0 0 20 20" fill="currentColor">
+		<path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+	</svg>
+	<!-- sun icon -->
+	<svg class="size-7 text-yellow-400" class:hidden={dark} viewBox="0 0 20 20" fill="currentColor">
+		<path
+			fill-rule="evenodd"
+			d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+			clip-rule="evenodd"
+		/>
+	</svg>
+</button>
