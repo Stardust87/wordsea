db57ea9353e2dccd5f58126a7ea9524b44f11f07
Anna Klimiuk <ania.klimiuk411@gmail.com>
2024-02-19 16:22:24 +0100

    app: display random image from chosen daily word

--- app/src/lib/components/dailyImage.svelte
+++ app/src/lib/components/dailyImage.svelte
@@ -3,18 +3,20 @@
 	export let mnemonics: Mnemonic[] = [];
 	export let randomWord: string = '';
 
-	$: featured = mnemonics[0];
+	$: featured = mnemonics[Math.floor(Math.random() * mnemonics.length)];
 </script>
 
-<div class="relative left-1/2 mb-4 flex -translate-x-1/2 justify-center md:w-1/2">
-	<img
-		class="h-auto max-w-full rounded-lg shadow-lg duration-300"
-		src="data:image/webp;base64,{featured.images[0]}"
-		alt={featured.prompt}
-	/>
-	<div class="absolute bottom-0 left-0 flex w-full justify-center bg-slate-800/60 py-5">
-		<a href="/words/{randomWord}" class="text-center text-xl font-semibold text-slate-300"
-			>{randomWord}</a
+<div class="relative left-1/2 mb-4 flex -translate-x-1/2 justify-center rounded-lg md:w-1/2">
+	<a href="/words/{randomWord}" class="rounded-lg">
+		<img
+			class="h-auto max-w-full rounded-lg shadow-xl duration-300"
+			src="data:image/webp;base64,{featured.images[0]}"
+			alt={featured.prompt}
+		/>
+		<div
+			class="absolute bottom-0 left-0 flex w-full justify-center rounded-lg bg-slate-800/60 py-2 md:py-5"
 		>
-	</div>
+			<span class="text-center text-lg font-semibold text-slate-300 md:text-xl">{randomWord}</span>
+		</div>
+	</a>
 </div>
