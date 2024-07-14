e0a2ce4847cd69fe5f0ffea1317079274c1fd241
Michał Szachniewicz <mszachniewicz@outlook.com>
2024-07-14 12:34:50 +0200

    feat: set front page word via env

--- app/src/routes/+page.server.ts
+++ app/src/routes/+page.server.ts
@@ -2,21 +2,21 @@ import { mnemonics, baseWords } from '$lib/server/database';
 import type { Mnemonic } from '$lib/types/Mnemonic';
 import { loadImage } from '$lib/server/loader';
 
+import { PUBLIC_FRONT_WORD, PUBLIC_FRONT_WORD_IMAGE } from '$env/static/public';
+
 const random = (words: string[]) => {
 	const date = new Date();
 	return (date.getFullYear() * date.getDate() * (date.getMonth() + 1)) % words.length;
 };
 
 export const load = async ({ url }) => {
-	const dailyWord = baseWords[random(baseWords)];
+	const dailyWord = PUBLIC_FRONT_WORD || baseWords[random(baseWords)];
+	const dailyMnemonics = await mnemonics.find({ word: dailyWord }).sort({ $natural: -1 }).toArray();
 
-	const dailyMnemonics = await mnemonics
-		.find({ word: dailyWord })
-		.sort({ $natural: -1 })
-		.limit(1)
-		.toArray();
+	let dailyImageIndex = Number(PUBLIC_FRONT_WORD_IMAGE) || 0;
+	dailyImageIndex = dailyImageIndex % dailyMnemonics.length;
 
-	const featured = dailyMnemonics[0];
+	const featured = dailyMnemonics[dailyImageIndex];
 	featured.image = await loadImage(featured.image);
 
 	const ogImageUrl = `${url.origin}/api/image?word=${dailyWord}&index=0`;
